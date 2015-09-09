c12f9a1135d29c729e68d9b9b515eb3eeab8287d
Ge Hao <althoughghgh@gmail.com>
2015-09-09 17:26:52 +0800

    fine

--- app/workers/dispatch_messages_worker.rb
+++ app/workers/dispatch_messages_worker.rb
@@ -3,10 +3,6 @@ class DispatchMessagesWorker
   sidekiq_options :retry => false, :backtrace => true
 
   def perform(msg_id)
-    Inbox.transaction do
-      User.all.each do |user|
-        Inbox.create(:message_id => msg_id, :user_id => user.id)
-      end
-    end
+    $redis.setbit("global-msg#{msg_id}", 0, 1)
   end
 end
--- script/ruby.rb
+++ script/ruby.rb
@@ -1,35 +1,58 @@
-require 'mysql'
+# 2015-08-25 19:09:52 +0800
+# 101.236374
+# 102.595443
+# 2015-08-25 19:11:35 +0800
 
-con = Mysql.new 'localhost', 'root', '', 'domain_noti_development'
+require 'mysql2'
+
+
+client = Mysql2::Client.new(
+  :host => 'localhost',
+  :username => 'root',
+  :password => '123654789',
+  :database => 'domain_noti_development',
+  :encoding => 'utf8',
+  :read_timeout => 600,
+  :write_timeout => 600,
+  :connect_timeout => 600,
+  :reconnect => true
+)
+
+
+
 
 p c = Time.now
 
-start_transaction = "START TRANSACTION; "
-commit = "COMMIT;"
 
+query_string = "insert into users (id, name, email, created_at, updated_at) values "
 
-con.query start_transaction
-for id in 1..1_000_000
+#con.query start_transaction
+for id in 1..1000_000
 
   name = "\'user-#{id}\'"
   email = "\'although#{id}@gmail.com\'"
   created_at = updated_at = Time.now
 
-  con.query "insert into users (id, name, email, created_at, updated_at) values (#{id}, #{name}, #{email}, \'#{created_at}\', \'#{updated_at}\'); "
+  query_string << "(#{id}, #{name}, #{email}, \'#{created_at}\', \'#{updated_at}\'),"
 end
 
-f = Time.now - c
 
-con.query commit
+p f = Time.now - c
 
-con.close
+query_string[-1] = ";"
 
-p f
+
+client.query(query_string)
+
+
+#con.query query_string
+#con.query commit
+
+#con.close
+
 p d = Time.now - c
 p Time.now
 
 
-# 2015-08-25 19:09:52 +0800
-# 101.236374
-# 102.595443
-# 2015-08-25 19:11:35 +0800
+
+
